ca16c515a4fb7b29b5b0b923a61624cedcc92ab2
AnushaUppuu <anusha.uppu@everest.engineering>
2025-07-04 16:12:16 +0530

    Add the search_flights method

--- app/controllers/flights_controller.rb
+++ app/controllers/flights_controller.rb
@@ -16,6 +16,18 @@ class FlightsController < ApplicationController
     flights[0]["price"] = 9999
 
     File.write(FILE_PATH, JSON.pretty_generate(flights))
-    redirect_to root_path, notice: "Flight data updated!"
+    redirect_to root_path, notice: "Flight data updated successfully!"
+  end
+  def search
+    if params[:query].blank? || params[:origin].blank?
+      flash[:alert] = "Please provide both destination and origin."
+      redirect_to root_path and return
+    end
+    file_content = File.read(FILE_PATH)
+    flights = JSON.parse(file_content)
+
+    # Example Search: Find flights to a specific destination
+    search_results = flights.select { |flight| flight["destination"].downcase.include?(params[:query].downcase) && flight["origin"].downcase.include?(params[:origin].downcase) }
+    render json: search_results 
   end
 end
