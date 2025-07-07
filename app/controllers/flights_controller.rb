70ae1a1106a3bb81d12e4eff51137faa44d51b21
Shailaja Nimmagari <shailaja.nimmagari@everest.engineering>
2025-07-07 12:00:10 +0530

    Add results controller for getting the flights

--- app/controllers/flights_controller.rb
+++ app/controllers/flights_controller.rb
@@ -24,4 +24,11 @@ class FlightsController < ApplicationController
     search_results = flights.select { |flight| flight["destination"].downcase.include?(params[:query].downcase) && flight["source"].downcase.include?(params[:origin].downcase) }
     render json: search_results
   end
+  def results
+  file_content = File.read(FILE_PATH)
+  flights = JSON.parse(file_content)
+
+  @search_results = flights  
+  render :details
+  end
 end
