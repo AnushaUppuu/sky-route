f0fd90124268ea82b198eeaf04743f6e03c42d9e
Mamatha Niyal <mamatha.niyal@everest.engineering>
2025-07-07 16:19:29 +0530

    Update reading comma-separated values

--- app/controllers/flights_controller.rb
+++ app/controllers/flights_controller.rb
@@ -1,4 +1,4 @@
-require 'json'
+require 'csv'
 
 class FlightsController < ApplicationController
   FILE_PATH = Rails.root.join('data', 'data.txt')
@@ -6,26 +6,34 @@ class FlightsController < ApplicationController
   def index
   end
 
-  def update
-    file_content = File.read(FILE_PATH)
-    flights = JSON.parse(file_content)
-    flights[0]["price"] = 9999
-    File.write(FILE_PATH, JSON.pretty_generate(flights))
-    redirect_to root_path, notice: "Flight data updated successfully!"
-  end
-  def search
+   def search
     render :search
   end
+
+  def load_flights_from_txt
+
+    flights = []
+    CSV.foreach(FILE_PATH, headers: true) do |row|
+      flights << row.to_h.symbolize_keys
+    end
+  
+    flights
+  end
+
   def details
-    if params[:source].present? && params[:destination].present?
-      file_content = File.read(FILE_PATH)
-      flights = JSON.parse(file_content)
+    if params[:source].present? && params[:destination].present? && params[:departure_date].present?
+      flights = load_flights_from_txt
       search_results = flights.select do |flight|
-        flight["destination"].downcase.include?(params[:destination].downcase) &&
-        flight["source"].downcase.include?(params[:source].downcase)
+        flight[:destination].to_s.downcase.include?(params[:destination].downcase) &&
+        flight[:source].to_s.downcase.include?(params[:source].downcase) && 
+          flight[:departure_date].to_s.downcase.include?(params[:departure_date].downcase)
       end
-      @search_results = search_results
+      @search_results = search_results.presence || []
+    else 
+      @search_results = []
     end
     render :details
   end
+
+  
 end
