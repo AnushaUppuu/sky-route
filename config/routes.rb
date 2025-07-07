1141e7690558fb1ae9a7d553703292bc6568872d
AnushaUppuu <anusha.uppu@everest.engineering>
2025-07-07 13:39:21 +0530

    Modify the results route to details

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
    root "flights#index"
   resources :flights, only: [ :index ] do
     collection do
-      get :results
+      get :details
       get :search
       post :update
     end
