9d2c5f45c0f4b8737818053b101d97dc591f4fab
AnushaUppuu <anusha.uppu@everest.engineering>
2025-07-08 11:13:28 +0530

    Update the total_fare_calculate function, add the days_based_price on the resultant price

--- app/helpers/flights_helper.rb
+++ app/helpers/flights_helper.rb
@@ -15,29 +15,34 @@ module FlightsHelper
       base_price
     end
   end
-
-  def calculate_total_fare(total_seats, available_seats, base_price, passengers)
+  def daysBefore(date)
+    return 0 if date.nil? || date.empty?
+    begin
+      flight_date = Date.parse(date)
+      (flight_date - Date.today).to_i
+    rescue ArgumentError
+      0
+    end
+  end
+  def days_based_price(days, base_price)
+    if days>15
+      base_price
+    elsif days <= 15 && days >= 3
+      base_price + (0.2 * base_price)* days
+    else
+      base_price + (0.1 * base_price)* days
+    end
+  end
+  def calculate_total_fare(total_seats, available_seats, base_price, passengers, date)
     return 0 if total_seats.to_i == 0
     available_percentage = percentage_seats_available(total_seats, available_seats)
     price_per_seat = seat_based_price(available_percentage, base_price)
-    (price_per_seat * passengers).round(2)
+
+    total_fare = price_per_seat * passengers
+
+    days = daysBefore(date)
+    total_fare_with_date_pricing = days_based_price(days, total_fare)
+
+    total_fare_with_date_pricing.round(2)
   end
-    def daysBefore(date)
-        return 0 if date.nil? || date.empty?
-        begin
-          flight_date = Date.parse(date)
-          (flight_date - Date.today).to_i
-        rescue ArgumentError
-          0
-        end
-      end
-      def days_based_price(days, base_price)
-        if days>15
-          base_price
-        elsif days <= 15 && days >= 3
-          base_price + (0.2 * base_price)* days
-        else
-          base_price + (0.1 * base_price)* days
-        end
-      end
 end
