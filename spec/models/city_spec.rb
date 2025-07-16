d5a496c424dafcb26d4131d72e6d9cdc38125952
AnushaUppuu <anusha.uppu@everest.engineering>
2025-07-16 23:54:58 +0530

    Add basic tests

--- spec/models/city_spec.rb
+++ spec/models/city_spec.rb
@@ -1,5 +1,8 @@
+# spec/models/city_spec.rb
 require 'rails_helper'
 
 RSpec.describe City, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it "is a valid dummy test" do
+    expect(true).to be true
+  end
 end
--- spec/models/flight_spec.rb
+++ spec/models/flight_spec.rb
@@ -1,5 +1,8 @@
+# spec/models/flight_spec.rb
 require 'rails_helper'
 
 RSpec.describe Flight, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it "is a valid dummy test" do
+    expect(true).to be true
+  end
 end
