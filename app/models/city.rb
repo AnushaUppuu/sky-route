53a50165a0ad972b2b2c8a8c3ae5b5803b7775c5
AnushaUppuu <anusha.uppu@everest.engineering>
2025-07-16 23:54:39 +0530

    Remove commented lines

--- app/models/city.rb
+++ app/models/city.rb
@@ -1,4 +1,4 @@
-# app/models/city.rb
+
 class City < ApplicationRecord
   has_many :departing_flights, class_name: "Flight", foreign_key: "source_city_id"
   has_many :arriving_flights, class_name: "Flight", foreign_key: "destination_city_id"
