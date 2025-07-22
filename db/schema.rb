c4ce325512ec224e3a45ed61c5cb292b5bbfb0cb
varun-martha <varunkumar.martha@everest.engineering>
2025-07-22 16:39:37 +0530

    Update the schema, add the recurrance and decompose the flight table

--- db/schema.rb
+++ db/schema.rb
@@ -10,8 +10,8 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 2025_07_16_171550) do
-  execute "CREATE SCHEMA IF NOT EXISTS auth"
+ActiveRecord::Schema[8.0].define(version: 2025_07_22_091439) do
+  create_schema "auth"
   create_schema "extensions"
   create_schema "graphql"
   create_schema "graphql_public"
@@ -28,35 +28,102 @@ ActiveRecord::Schema[8.0].define(version: 2025_07_16_171550) do
   enable_extension "pg_catalog.plpgsql"
   enable_extension "vault.supabase_vault"
 
-  create_table "cities", force: :cascade do |t|
+  create_table "airlines", force: :cascade do |t|
+    t.string "name"
+    t.string "code"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "airports", force: :cascade do |t|
+    t.string "name"
+    t.string "code"
+    t.string "city"
+    t.string "country"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "flight_classes", force: :cascade do |t|
     t.string "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.index ["name"], name: "index_cities_on_name", unique: true
+  end
+
+  create_table "flight_custom_dates", force: :cascade do |t|
+    t.bigint "flight_id", null: false
+    t.date "custom_date"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["flight_id"], name: "index_flight_custom_dates_on_flight_id"
+  end
+
+  create_table "flight_schedules", force: :cascade do |t|
+    t.bigint "flight_id", null: false
+    t.time "departure_time"
+    t.time "arrival_time"
+    t.string "status"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["flight_id"], name: "index_flight_schedules_on_flight_id"
+  end
+
+  create_table "flight_seats", force: :cascade do |t|
+    t.bigint "flight_schedule_id", null: false
+    t.bigint "flight_class_id", null: false
+    t.integer "total_seats"
+    t.integer "available_seats"
+    t.decimal "price"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["flight_class_id"], name: "index_flight_seats_on_flight_class_id"
+    t.index ["flight_schedule_id"], name: "index_flight_seats_on_flight_schedule_id"
+  end
+
+  create_table "flight_special_dates", force: :cascade do |t|
+    t.bigint "flight_id", null: false
+    t.date "special_date"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["flight_id"], name: "index_flight_special_dates_on_flight_id"
+  end
+
+  create_table "flight_weekdays", force: :cascade do |t|
+    t.bigint "flight_id", null: false
+    t.string "day_of_week"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["flight_id"], name: "index_flight_weekdays_on_flight_id"
   end
 
   create_table "flights", force: :cascade do |t|
-    t.string "airlines"
+    t.bigint "airline_id", null: false
     t.string "flight_number"
-    t.bigint "source_city_id", null: false
-    t.bigint "destination_city_id", null: false
-    t.decimal "economy_base_price"
-    t.decimal "first_class_base_price"
-    t.decimal "second_class_base_price"
-    t.integer "economy_total_seats"
-    t.integer "first_class_total_seats"
-    t.integer "second_class_total_seats"
-    t.integer "economy_available_seats"
-    t.integer "first_class_available_seats"
-    t.integer "second_class_available_seats"
-    t.date "departure_date"
-    t.time "departure_time"
-    t.date "arrival_date"
-    t.time "arrival_time"
+    t.bigint "source_airport_id", null: false
+    t.bigint "destination_airport_id", null: false
+    t.bigint "recurrence_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["airline_id"], name: "index_flights_on_airline_id"
+    t.index ["destination_airport_id"], name: "index_flights_on_destination_airport_id"
+    t.index ["recurrence_id"], name: "index_flights_on_recurrence_id"
+    t.index ["source_airport_id"], name: "index_flights_on_source_airport_id"
+  end
+
+  create_table "recurrences", force: :cascade do |t|
+    t.string "recurrence_type"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
 
-  add_foreign_key "flights", "cities", column: "destination_city_id"
-  add_foreign_key "flights", "cities", column: "source_city_id"
+  add_foreign_key "flight_custom_dates", "flights"
+  add_foreign_key "flight_schedules", "flights"
+  add_foreign_key "flight_seats", "flight_classes"
+  add_foreign_key "flight_seats", "flight_schedules"
+  add_foreign_key "flight_special_dates", "flights"
+  add_foreign_key "flight_weekdays", "flights"
+  add_foreign_key "flights", "airlines"
+  add_foreign_key "flights", "airports", column: "destination_airport_id"
+  add_foreign_key "flights", "airports", column: "source_airport_id"
+  add_foreign_key "flights", "recurrences"
 end
