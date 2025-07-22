4999feb63ea3e29708f0ec8c61e3b95081c1d892
varun-martha <varunkumar.martha@everest.engineering>
2025-07-22 16:29:13 +0530

    Delete the city and flight old migrations

--- db/migrate/20250717113708_add_available_seats_to_flights.rb
+++
@@ -1,7 +0,0 @@
-class AddAvailableSeatsToFlights < ActiveRecord::Migration[8.0]
-  def change
-    add_column :flights, :economy_available_seats, :integer
-    add_column :flights, :first_class_available_seats, :integer
-    add_column :flights, :second_class_available_seats, :integer
-  end
-end
