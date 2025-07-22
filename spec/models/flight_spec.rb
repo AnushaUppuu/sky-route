19e96b8e1c1f58f3eec8a881157fd599c1746693
varun-martha <varunkumar.martha@everest.engineering>
2025-07-22 16:40:16 +0530

    Update the minimal test of the flight model

--- spec/models/flight_spec.rb
+++ spec/models/flight_spec.rb
@@ -1,8 +1,20 @@
-# spec/models/flight_spec.rb
 require 'rails_helper'
 
 RSpec.describe Flight, type: :model do
-  it "is a valid dummy test" do
-    expect(true).to be true
+  it "creates a valid flight with associated records" do
+    airline = Airline.create(name: "Test Airline", code: "TA")
+    source = Airport.create(name: "Source Airport", code: "SRC", city: "CityA", country: "CountryA")
+    destination = Airport.create(name: "Destination Airport", code: "DST", city: "CityB", country: "CountryB")
+    recurrence = Recurrence.create(recurrence_type: "daily")
+
+    flight = Flight.new(
+      airline: airline,
+      source_airport: source,
+      destination_airport: destination,
+      recurrence: recurrence,
+      flight_number: "TA123"
+    )
+
+    expect(flight.save).to be true
   end
 end
