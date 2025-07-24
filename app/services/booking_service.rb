5375e6a73e8cb4044dc480360130ae733e8377bd
AnushaUppuu <anusha.uppu@everest.engineering>
2025-07-24 22:56:55 +0530

    Remove commented lines

--- app/services/booking_service.rb
+++ app/services/booking_service.rb
@@ -1,4 +1,3 @@
-# app/services/booking_service.rb
 class BookingService
   attr_reader :params, :status, :error_message
 
@@ -9,9 +8,8 @@ class BookingService
   end
 
   def process_booking
-    flight_number, class_type, passengers = extract_booking_params
-
-    return error(:bad_request, "Invalid booking details") if flight_number.blank? || class_type.blank? || passengers <= 0
+    flight_number, class_type, passengers, scheduled_date = extract_booking_params
+    return error(:bad_request, "Invalid booking details") if flight_number.blank? || class_type.blank? || passengers <= 0 || scheduled_date.blank?
 
     flight = Flight.find_by(flight_number: flight_number)
     return error(:not_found, "Flight not found") unless flight
@@ -25,10 +23,13 @@ class BookingService
     seat = FlightSeat.find_by(flight_schedule_id: schedule.id, flight_class_id: flight_class.id)
     return error(:not_found, "Given class is not available for the flight") unless seat
 
-    FlightSeat.transaction do
-      seat.lock!
-      if seat.available_seats >= passengers
-        seat.update!(available_seats: seat.available_seats - passengers)
+    seat_availability = FlightSeatAvailability.find_by(flight_seat_id: seat.id, scheduled_date: scheduled_date)
+    return error(:not_found, "No seat availability for the selected date") unless seat_availability
+
+    FlightSeatAvailability.transaction do
+      seat_availability.lock!
+      if seat_availability.available_seats >= passengers
+        seat_availability.update!(available_seats: seat_availability.available_seats - passengers)
         return { message: "Booking Successful" }
       else
         return error(:unprocessable_entity, "Not enough seats available")
@@ -45,11 +46,13 @@ class BookingService
   end
 
   def extract_booking_params
-    permitted = params.permit(:flight_number, :class_type, :passengers)
+    puts "Extracting booking params: #{params.inspect}"
+    permitted = params.permit(:flight_number, :class_type, :passengers, :scheduled_date)
     [
       permitted[:flight_number],
       permitted[:class_type]&.strip,
-      permitted[:passengers].to_i
+      permitted[:passengers].to_i,
+      permitted[:scheduled_date]
     ]
   end
 end
