b9ceb289a6519bfb644b2a646746ef55585c6df4
AnushaUppuu <anusha.uppu@everest.engineering>
2025-07-24 12:04:42 +0530

    Update the test cases for the search action

--- spec/requests/api/v1/flights_spec.rb
+++ spec/requests/api/v1/flights_spec.rb
@@ -1,196 +1,227 @@
 require 'rails_helper'
 
-RSpec.describe "Api::V1::FlightsController - details", type: :request do
-  let!(:source_city) { City.create!(name: "Delhi") }
-  let!(:destination_city) { City.create!(name: "Mumbai") }
-    let!(:another_city) { City.create!(name: "Thirupathi") }
+RSpec.describe "Api::V1::FlightsController", type: :request do
+  describe "POST /api/v1/flights/search" do
+    let!(:source_airport) { Airport.create!(code: "DEL", name: "Delhi Airport", city: "Delhi") }
+    let!(:destination_airport) { Airport.create!(code: "BOM", name: "Mumbai Airport", city: "Mumbai") }
+    let!(:airline) { Airline.create!(name: "Air India") }
+    let!(:recurrence) { Recurrence.create!(recurrence_type: "daily") }
+    let!(:flight_class) { FlightClass.create!(name: "Economy") }
 
-  let!(:flight) do
-    Flight.create!(
-      airlines: "AirIndia",
-      flight_number: "AI101",
-      source_city: source_city,
-      destination_city: destination_city,
-      economy_base_price: 4500,
-      first_class_base_price: 9000,
-      second_class_base_price: 7000,
-      economy_total_seats: 100,
-      first_class_total_seats: 20,
-      second_class_total_seats: 30,
-      economy_available_seats: 5,
-      first_class_available_seats: 10,
-      second_class_available_seats: 15,
-      departure_date: "2025-07-20",
-      departure_time: "10:00",
-      arrival_date: "2025-07-20",
-      arrival_time: "12:00"
-    )
+    let!(:flight) do
+      Flight.create!(
+        flight_number: "AI123",
+        source_airport: source_airport,
+        destination_airport: destination_airport,
+        airline: airline,
+        recurrence: recurrence
+      )
+    end
+
+    let!(:flight_schedule) do
+      FlightSchedule.create!(
+        flight: flight,
+        departure_time: Time.now + 1.day,
+        arrival_time: Time.now + 1.day + 2.hours
+      )
+    end
+
+    let!(:flight_seat) do
+      FlightSeat.create!(
+        flight_schedule: flight_schedule,
+        flight_class: flight_class,
+        total_seats: 100,
+        available_seats: 95,
+        price: 5000
+      )
+    end
+
+    it "Should returns available flights with total cost" do
+      post "/api/v1/flights/search", params: {
+        source: "DEL",
+        destination: "BOM",
+        departure_date: Date.today.to_s,
+        passengers: 1,
+        class_type: "Economy",
+        currency: "INR"
+      }
+      expect(response).to have_http_status(:ok)
+      json = JSON.parse(response.body)
+      expect(json).to be_an(Array)
+      expect(json.first).to have_key("total_cost")
+    end
+
+    it "Should returns error when source and destination are the same" do
+      post "/api/v1/flights/search", params: {
+        source: "DEL",
+        destination: "DEL",
+        departure_date: Date.today.to_s,
+        passengers: 1,
+        class_type: "Economy",
+        currency: "INR"
+      }
+      expect(response).to have_http_status(:bad_request)
+      json = JSON.parse(response.body)
+      expect(json["error"]).to eq("Source and destination cannot be the same")
+    end
+
+    it "Should returns error when source is missing" do
+      post "/api/v1/flights/search", params: {
+        destination: "BOM",
+        departure_date: Date.today.to_s,
+        passengers: 1,
+        class_type: "Economy",
+        currency: "INR"
+      }
+      expect(response).to have_http_status(:bad_request)
+      json = JSON.parse(response.body)
+      expect(json["error"]).to eq("Source and destination are required")
+    end
+
+    it "should returns error when destination is missing" do
+      post "/api/v1/flights/search", params: {
+        source: "DEL",
+        departure_date: Date.today.to_s,
+        passengers: 1,
+        class_type: "Economy",
+        currency: "INR"
+      }
+      expect(response).to have_http_status(:bad_request)
+      json = JSON.parse(response.body)
+      expect(json["error"]).to eq("Source and destination are required")
+    end
+
+    it "Should returns error when departure date is invalid" do
+      post "/api/v1/flights/search", params: {
+        source: "DEL",
+        destination: "BOM",
+        departure_date: "not-a-date",
+        passengers: 1,
+        class_type: "Economy",
+        currency: "INR"
+      }
+      expect(response).to have_http_status(:bad_request)
+      json = JSON.parse(response.body)
+      expect(json["error"]).to eq("Invalid departure date")
+    end
+
+    it "Should returns error when source or destination airport code is invalid" do
+        post "/api/v1/flights/search", params: {
+            source: "XXX",
+            destination: "BOM",
+            departure_date: Date.today.to_s,
+            passengers: 1,
+            class_type: "Economy",
+            currency: "INR"
+        }
+        expect(response).to have_http_status(:bad_request)
+        json = JSON.parse(response.body)
+        expect(json["error"]).to eq("Invalid source or destination airport")
+    end
+
+    it "Should returns error when class type is invalid" do
+        post "/api/v1/flights/search", params: {
+            source: "DEL",
+            destination: "BOM",
+            departure_date: Date.today.to_s,
+            passengers: 1,
+            class_type: "InvalidClass",
+            currency: "INR"
+        }
+        expect(response).to have_http_status(:bad_request)
+        json = JSON.parse(response.body)
+        expect(json["error"]).to eq("Invalid class type")
+    end
+
+    it "Should returns error when no flights exist between source and destination" do
+        unknown_airport1 = Airport.create!(city: "Hyderabad", code: "ZZZ", name: "ZZZ Airport")
+        unknown_airport2 = Airport.create!(city: "Delhi", code: "YYY", name: "YYY Airport")
+        post "/api/v1/flights/search", params: {
+            source: "ZZZ",
+            destination: "YYY",
+            departure_date: Date.today.to_s,
+            passengers: 1,
+            class_type: "Economy",
+            currency: "INR"
+        }
+        expect(response).to have_http_status(:not_found)
+        expect(JSON.parse(response.body)["error"]).to eq("No flights between the selected route")
+    end
+
+    it "Should returns error when no flights are available on the selected date" do
+        flight.update!(recurrence: Recurrence.create!(recurrence_type: "weekly"))
+        FlightWeekday.delete_all
+        post "/api/v1/flights/search", params: {
+            source: "DEL",
+            destination: "BOM",
+            departure_date: Date.today.to_s,
+            passengers: 1,
+            class_type: "Economy",
+            currency: "INR"
+        }
+
+        expect(response).to have_http_status(:not_found)
+        json = JSON.parse(response.body)
+        expect(json["error"]).to eq("No flights available on the selected date")
+    end
+
+    it "Should returns error when no flight schedules exist for matched flights" do
+        FlightSeat.delete_all
+        FlightSchedule.delete_all
+        post "/api/v1/flights/search", params: {
+            source: "DEL",
+            destination: "BOM",
+            departure_date: Date.today.to_s,
+            passengers: 1,
+            class_type: "Economy",
+            currency: "INR"
+        }
+        expect(response).to have_http_status(:not_found)
+        json = JSON.parse(response.body)
+        expect(json["error"]).to eq("No flight schedules available on the selected date")
+    end
+
+
+    it "Should returns error when no seats available in selected class" do
+        flight_seat.update!(available_seats: 0)
+        post "/api/v1/flights/search", params: {
+            source: "DEL",
+            destination: "BOM",
+            departure_date: Date.today.to_s,
+            passengers: 1,
+            class_type: "Economy",
+            currency: "INR"
+        }
+        expect(response).to have_http_status(:not_found)
+        json = JSON.parse(response.body)
+        expect(json["error"]).to eq("No flights available for 1 travelers")
+    end
+
+    it "Should returns error when available seats are less than number of passengers" do
+        flight_seat.update!(available_seats: 2)
+        post "/api/v1/flights/search", params: {
+            source: "DEL",
+            destination: "BOM",
+            departure_date: Date.today.to_s,
+            passengers: 3,
+            class_type: "Economy",
+            currency: "INR"
+        }
+        expect(response).to have_http_status(:not_found)
+        json = JSON.parse(response.body)
+        expect(json["error"]).to eq("No flights available for 3 travelers")
+    end
+
+    it "Should uses default values when class, passengers, or currency are missing" do
+        post "/api/v1/flights/search", params: {
+            source: "DEL",
+            destination: "BOM",
+            departure_date: Date.today.to_s
+        }
+        expect(response).to have_http_status(:ok)
+        json = JSON.parse(response.body)
+        expect(json.first["class_type"]).to eq("economy")
+        expect(json.first["currency"]).to eq("INR")
+    end
   end
-
-  # describe "Tests related to the POST /api/v1/flights/details route" do
-  #   it "Should returns matching flights with valid params" do
-  #     post "/api/v1/flights/details", params: {
-  #       source: "Delhi",
-  #       destination: "Mumbai",
-  #       departure_date: "2025-07-20",
-  #       passengers: 1,
-  #       class_type: "economy"
-  #     }
-  #     expect(response).to have_http_status(:ok)
-  #     body = JSON.parse(response.body)
-  #     expect(body["data"]).to be_an(Array)
-  #     expect(body["data"].first["flight_number"]).to eq("AI101")
-  #     expect(body["message"]).to eq("Flights fetched successfully")
-  #   end
-
-  #   it "Should returns error when source and destination are the same" do
-  #     post "/api/v1/flights/details", params: {
-  #       source: "Delhi",
-  #       destination: "Delhi"
-  #     }
-  #     expect(response).to have_http_status(:bad_request)
-  #     body = JSON.parse(response.body)
-  #     expect(body["error"]).to eq("Source and destination cannot be the same")
-  #   end
-
-  #   it "Should returns error when required params are missing" do
-  #     post "/api/v1/flights/details", params: { destination: "Mumbai" }
-  #     expect(response).to have_http_status(:bad_request)
-  #     body = JSON.parse(response.body)
-  #     expect(body["error"]).to eq("Source and destination are required")
-  #   end
-
-  #   it "Should returns error when no matching flights found" do
-  #     post "/api/v1/flights/details", params: {
-  #       source: "Hyderabad",
-  #       destination: "Chennai"
-  #     }
-  #     expect(response).to have_http_status(:bad_request)
-  #     body = JSON.parse(response.body)
-  #     expect(body["error"]).to eq("Invalid source or destination city")
-  #   end
-
-  #   it "Should returns error when not enough seats available for passengers" do
-  #     post "/api/v1/flights/details", params: {
-  #       source: "Delhi",
-  #       destination: "Mumbai",
-  #       departure_date: "2025-07-20",
-  #       passengers: 100,
-  #       class_type: "economy"
-  #     }
-  #     expect(response).to have_http_status(:not_found)
-  #     body = JSON.parse(response.body)
-  #     expect(body["error"]).to eq("No flights available for 100 travelers")
-  #   end
-
-  #   it "Should calculates price using first class base price when class_type is 'first class'" do
-  #     post "/api/v1/flights/details", params: {
-  #       source: "Delhi",
-  #       destination: "Mumbai",
-  #       departure_date: "2025-07-20",
-  #       passengers: 1,
-  #       class_type: "first class"
-  #     }
-  #     expect(response).to have_http_status(:ok)
-  #     body = JSON.parse(response.body)
-  #     expect(body["data"].first["class_type"]).to eq("first class")
-  #     expect(body["data"].first["price_per_ticket"]).to eq(9000.0)
-  #   end
-
-  #   it "Should calculates price using second class base price when class_type is 'second class'" do
-  #     post "/api/v1/flights/details", params: {
-  #       source: "Delhi",
-  #       destination: "Mumbai",
-  #       departure_date: "2025-07-20",
-  #       passengers: 1,
-  #       class_type: "second class"
-  #     }
-  #     expect(response).to have_http_status(:ok)
-  #     body = JSON.parse(response.body)
-  #     expect(body["data"].first["class_type"]).to eq("second class")
-  #     expect(body["data"].first["price_per_ticket"]).to eq(7000.0)
-  #   end
-  #   it "Should return the error message when there are no flight between source and destination" do
-  #     post "/api/v1/flights/details", params: {
-  #       source: "Delhi",
-  #       destination: "Thirupathi",
-  #       passengers: 2,
-  #       class_type: "second class"
-  #     }
-  #     expect(response).to have_http_status(:not_found)
-  #     body=JSON.parse(response.body)
-  #     expect(body["error"]).to eq("There are no flights operated from this source to destination")
-  #   end
-  # end
-
-  # describe "Tests related to the PATCH /api/v1/flights/update_seat_count count" do
-  #   let(:endpoint) { "/api/v1/flights/update_seat_count" }
-
-  #   it "Should book seats if available and reduces available count" do
-  #     patch endpoint, params: {
-  #       flight_number: "AI101",
-  #       class_type: "economy",
-  #       passengers: 2
-  #     }
-  #     expect(response).to have_http_status(:ok)
-  #     body = JSON.parse(response.body)
-  #     expect(body["message"]).to eq("Booking successful")
-  #     expect(flight.reload.economy_available_seats).to eq(3)
-  #   end
-
-  #   it "Should returns error if not enough seats" do
-  #     patch endpoint, params: {
-  #       flight_number: "AI101",
-  #       class_type: "economy",
-  #       passengers: 100
-  #     }
-  #     expect(response).to have_http_status(:unprocessable_entity)
-  #     body = JSON.parse(response.body)
-  #     expect(body["error"]).to eq("Not enough seats available")
-  #   end
-
-  #   it "Should returns error if flight is not found" do
-  #     patch endpoint, params: {
-  #       flight_number: "ABCD",
-  #       class_type: "economy",
-  #       passengers: 1
-  #     }
-  #     expect(response).to have_http_status(:not_found)
-  #     body = JSON.parse(response.body)
-  #     expect(body["error"]).to eq("Flight not found for updating seats")
-  #   end
-
-  #   it "Should returns error if invalid class_type is provided" do
-  #     patch endpoint, params: {
-  #       flight_number: "AI101",
-  #       class_type: "business",
-  #       passengers: 1
-  #     }
-  #     expect(response).to have_http_status(:bad_request)
-  #     body = JSON.parse(response.body)
-  #     expect(body["error"]).to eq("Invalid class type")
-  #   end
-
-  #   it "Should returns error if passengers param is missing or invalid" do
-  #     patch endpoint, params: {
-  #       flight_number: "AI101",
-  #       class_type: "economy",
-  #       passengers: 0
-  #     }
-  #     expect(response).to have_http_status(:bad_request)
-  #     body = JSON.parse(response.body)
-  #     expect(body["error"]).to eq("Invalid booking details")
-  #   end
-
-  #   it "Should returns error if flight_number is missing" do
-  #     patch endpoint, params: {
-  #       flight_number: "",
-  #       class_type: "economy",
-  #       passengers: 1
-  #     }
-  #     expect(response).to have_http_status(:bad_request)
-  #     body = JSON.parse(response.body)
-  #     expect(body["error"]).to eq("Invalid booking details")
-  #   end
-  # end
 end
