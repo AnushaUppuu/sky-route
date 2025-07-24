21bd3e11d87067e2c590d1a3b7492bc6468228b9
varun-martha <varunkumar.martha@everest.engineering>
2025-07-24 13:13:05 +0530

    Add validation rules for FlightSchedule model

--- app/models/flight_schedule.rb
+++ app/models/flight_schedule.rb
@@ -1,3 +1,7 @@
 class FlightSchedule < ApplicationRecord
   belongs_to :flight
+  has_many :flight_seats, dependent: :destroy
+
+  validates :departure_time, :arrival_time, :status, presence: true
+  validates :flight_id, uniqueness: { scope: [:departure_time, :arrival_time] }
 end
