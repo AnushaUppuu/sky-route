574813d46f68d5485ccdca8139d24f3304b35802
varun-martha <varunkumar.martha@everest.engineering>
2025-07-24 13:11:10 +0530

    Add validation rules for Airport model

--- app/models/airport.rb
+++ app/models/airport.rb
@@ -1,2 +1,7 @@
 class Airport < ApplicationRecord
+  has_many :departing_flights, class_name: "Flight", foreign_key: :source_airport_id, dependent: :restrict_with_exception
+  has_many :arriving_flights, class_name: "Flight", foreign_key: :destination_airport_id, dependent: :restrict_with_exception
+
+  validates :name, presence: true
+  validates :code, presence: true, uniqueness: true
 end
