beaf7da3e845e6a62e50814960f862f297a8f4a8
varun-martha <varunkumar.martha@everest.engineering>
2025-07-24 13:10:49 +0530

    Add validations for airline model

--- app/models/airline.rb
+++ app/models/airline.rb
@@ -1,2 +1,6 @@
 class Airline < ApplicationRecord
+  has_many :flights, dependent: :restrict_with_exception
+
+  validates :name, presence: true
+  validates :code, presence: true, uniqueness: true
 end
