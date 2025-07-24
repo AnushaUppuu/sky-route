945c97937520674666c0d4758b3029f95b279969
varun-martha <varunkumar.martha@everest.engineering>
2025-07-24 13:13:47 +0530

    Add validation rules for FlightSeat model

--- app/models/flight_seat.rb
+++ app/models/flight_seat.rb
@@ -1,4 +1,9 @@
 class FlightSeat < ApplicationRecord
   belongs_to :flight_schedule
   belongs_to :flight_class
+  has_many :flight_seat_availabilities, dependent: :destroy
+
+  validates :total_seats, presence: true, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
+  validates :price, presence: true, numericality: { greater_than_or_equal_to: 0 }
+  validates :flight_schedule_id, uniqueness: { scope: :flight_class_id }
 end
